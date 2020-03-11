4aed340ed11148d8fd27841f313a7c5ee27f1ec2
Juliette Regimbal <juliette.regimbal@gmail.com>
2020-03-11 18:53:31 -0400

    Fix CORS issue in development

--- src/app/tools.ts
+++ src/app/tools.ts
@@ -3,7 +3,7 @@ import { IRI } from './input/definitions';
 export function formIIIFManifest(source: string, identifier: string): IRI {
   switch (source) {
     case 'gallica':
-      return 'https://gallica.bnf.fr/iiif/ark:/' +
+      return '//gallica.bnf.fr/iiif/ark:/' +
         identifier +
         '/manifest.json';
     default:

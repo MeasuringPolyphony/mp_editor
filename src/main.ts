e8100ea419ee159260fb85c548cffc08595b5d5e
Juliette Regimbal <juliette.regimbal@gmail.com>
2020-04-26 20:29:49 -0400

    Ensure verovio is loaded before starting

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,22 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+declare let verovioReady: boolean;
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+init().then( () => {
+  platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err))
+});
+
+async function init() {
+  console.debug('check');
+  while (!verovioReady) {
+    console.debug('wait');
+    await new Promise(r => { setTimeout(r, 250); });
+  }
+  console.debug('done');
+}

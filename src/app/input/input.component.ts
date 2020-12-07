cd3c2b656de0b12a96d3d82bafbf9e61d2571621
Juliette Regimbal <juliette.regimbal@gmail.com>
2020-12-07 15:48:32 -0500

    Get new contributor entry to appear

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormArray, FormControl } from '@angular/forms';
 
 import { IRI } from '../utils/definitions';
 import { MEIDocument } from '../utils/mei';
@@ -19,11 +19,18 @@ export class InputComponent implements OnInit {
   metadataForm = new FormGroup({
     shortTitle: new FormControl(''),
     composerName: new FormControl(''),
-    userName: new FormControl(''),
+    contributors: new FormArray([
+      new FormGroup({
+        type: new FormControl(''),
+        name: new FormControl(''),
+      })
+    ]),
     notationSubtype: new FormControl(''),
     siglum: new FormControl(''),
     genre: new FormControl(''),
   });
+
+  temp: FormArray;
 
   inputStep: InputComponent.InputStep;
 
@@ -46,13 +53,15 @@ export class InputComponent implements OnInit {
     if (this.stateService.mei.metadata != undefined) {
       this.metadataForm.controls.shortTitle.setValue(this.stateService.mei.metadata.shortTitle);
       this.metadataForm.controls.composerName.setValue(this.stateService.mei.metadata.composerName);
-      this.metadataForm.controls.userName.setValue(this.stateService.mei.metadata.encoderName);
+      //this.metadataForm.controls.userName.setValue(this.stateService.mei.metadata.encoderName);
       this.metadataForm.controls.notationSubtype.setValue(this.stateService.mei.notationSubtype);
       this.metadataForm.controls.siglum.setValue(this.stateService.mei.metadata.siglum);
       this.metadataForm.controls.genre.setValue(this.stateService.mei.metadata.genre);
     }
 
+    this.temp = this.metadataForm.get('contributors') as FormArray;
     console.debug(this.iiifManifest);
+    console.debug(this.metadataForm.controls.contributors);
   }
 
   ngOnInit() {
@@ -64,7 +73,8 @@ export class InputComponent implements OnInit {
     this.stateService.mei.metadata = {
       shortTitle: value.shortTitle,
       composerName: value.composerName,
-      encoderName: value.userName,
+      encoderName: "",
+      //encoderName: value.userName,
       sourceIRI: this.iiifManifest,
       siglum: value.siglum,
       genre: value.genre,
